a7d62ce44a2960051be88363c0c4edaab2d55a53
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-05-31 21:37:20 +0200

    added new working glaring eye

--- src/app/pages/css-animations-learning-page/css-animations-learning-page.component.ts
+++ src/app/pages/css-animations-learning-page/css-animations-learning-page.component.ts
@@ -3,11 +3,12 @@ import { BouncingBallComponent } from '../../animatedCSSComponents/movingBallPro
 import { MovingBallComponent } from '../../animatedCSSComponents/movingBallProgress/moving-ball/moving-ball.component';
 import { FollowingBallComponent } from '../../animatedCSSComponents/movingBallProgress/following-ball/following-ball/following-ball.component';
 import { GlaringBallComponent } from '../../animatedCSSComponents/movingBallProgress/glaring-ball/glaring-ball.component';
+import { GlaringBallComponentTry2 } from '../../animatedCSSComponents/movingBallProgress/glaring-ball-try2/glaring-ball-try2.component';
 
 @Component({
   selector: 'app-css-animations-learning-page',
   standalone: true,
-  imports: [BouncingBallComponent, MovingBallComponent, FollowingBallComponent, GlaringBallComponent],
+  imports: [BouncingBallComponent, MovingBallComponent, FollowingBallComponent, GlaringBallComponent, GlaringBallComponentTry2],
   templateUrl: './css-animations-learning-page.component.html',
   styleUrl: './css-animations-learning-page.component.css'
 })
