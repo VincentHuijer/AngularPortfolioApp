9f711be9c03711b4209dfa113341f9e5a2764c56
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-06-17 18:07:52 +0200

    first portfolio wip

--- src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
+++ src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AsterikDarkComponent } from '../../../assets/svgIcons/asterik-dark/asterik-dark.component';
 
 @Component({
   selector: 'app-vincent-portfolio',
   standalone: true,
-  imports: [],
+  imports: [AsterikDarkComponent],
   templateUrl: './vincent-portfolio.component.html',
   styleUrl: './vincent-portfolio.component.css'
 })
