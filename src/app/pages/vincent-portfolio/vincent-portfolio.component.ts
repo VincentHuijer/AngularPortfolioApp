5f27689179387d9e9c9fa5aa43e10e968845e216
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-06-18 15:50:52 +0200

    fininished Theon

--- src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
+++ src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AsterikDarkComponent } from '../../../assets/svgIcons/asterik-dark/asterik-dark.component';
 import { SkillBarComponent } from '../../generalComponents/skill-bar/skill-bar.component';
+import { LinkArrowComponent } from '../../../assets/svgIcons/link-arrow/link-arrow.component';
 
 @Component({
   selector: 'app-vincent-portfolio',
   standalone: true,
-  imports: [AsterikDarkComponent, SkillBarComponent],
+  imports: [AsterikDarkComponent, SkillBarComponent, LinkArrowComponent],
   templateUrl: './vincent-portfolio.component.html',
   styleUrl: './vincent-portfolio.component.css'
 })
