1481a2c818a1b4680c092c28ecd350a64331a95a
VincentHuijer <20161700@student.hhs.nl>
2024-06-20 14:55:51 +0200

    WIP before starting a chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'chat-bg2': "url('/assets/fotos/AIBackground.png')",
         'chat-bg3': 'linear-gradient(to right, #ff7e5f, #feb47b)',
         'chat-bg4': "url('/assets/fotos/Gradient1.png')",
+        'chat-bg5': "url('/assets/fotos/Gradient2.png')",
       })
     },
   },
