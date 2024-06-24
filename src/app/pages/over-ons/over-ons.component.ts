387c9f2f51d39dc7a30ba5ca4abca8fc532ce964
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-06-24 21:18:09 +0200

    created over ons page

--- src/app/pages/over-ons/over-ons.component.ts
+++ src/app/pages/over-ons/over-ons.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GlaringBall4Component } from '../../animatedCSSComponents/movingBallProgress/glaringBallProgress/glaring-ball4/glaring-ball4.component';
 
 @Component({
   selector: 'app-over-ons',
   standalone: true,
-  imports: [],
+  imports: [GlaringBall4Component],
   templateUrl: './over-ons.component.html',
   styleUrl: './over-ons.component.css'
 })
