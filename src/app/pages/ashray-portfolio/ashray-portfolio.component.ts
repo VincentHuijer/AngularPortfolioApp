e79777941f0d8c76c766a1ea3b76ae176fb5d2eb
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-06-24 23:02:49 +0200

    added my own portfolio

--- src/app/pages/ashray-portfolio/ashray-portfolio.component.ts
+++ src/app/pages/ashray-portfolio/ashray-portfolio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AsterikDarkComponent } from '../../../assets/svgIcons/asterik-dark/asterik-dark.component';
 import { SkillBarComponent } from '../../generalComponents/skill-bar/skill-bar.component';
 import { LinkArrowComponent } from '../../../assets/svgIcons/link-arrow/link-arrow.component';
+import { GlaringBall4Component } from '../../animatedCSSComponents/movingBallProgress/glaringBallProgress/glaring-ball4/glaring-ball4.component';
 
 @Component({
   selector: 'app-ashray-portfolio',
   standalone: true,
-  imports: [AsterikDarkComponent, SkillBarComponent, LinkArrowComponent],
+  imports: [AsterikDarkComponent, SkillBarComponent, LinkArrowComponent, GlaringBall4Component],
   templateUrl: './ashray-portfolio.component.html',
   styleUrl: './ashray-portfolio.component.css'
 })
--- src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
+++ src/app/pages/vincent-portfolio/vincent-portfolio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AsterikDarkComponent } from '../../../assets/svgIcons/asterik-dark/asterik-dark.component';
 import { SkillBarComponent } from '../../generalComponents/skill-bar/skill-bar.component';
 import { LinkArrowComponent } from '../../../assets/svgIcons/link-arrow/link-arrow.component';
+import { GlaringBall4Component } from '../../animatedCSSComponents/movingBallProgress/glaringBallProgress/glaring-ball4/glaring-ball4.component';
 
 @Component({
   selector: 'app-vincent-portfolio',
   standalone: true,
-  imports: [AsterikDarkComponent, SkillBarComponent, LinkArrowComponent],
+  imports: [AsterikDarkComponent, SkillBarComponent, LinkArrowComponent, GlaringBall4Component],
   templateUrl: './vincent-portfolio.component.html',
   styleUrl: './vincent-portfolio.component.css'
 })
